06506752fc63ac8a5efc4e0b9e9e153eac852a88
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-30 18:49:50 +0700

    navbar and footer

---
+++ Assignment_1/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Helee6eeeeeeeeeeee');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
